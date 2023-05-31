3c11a35a4e2d3d36305d3fa3be1cffe2215a5e16
Raxmatillo <ilmnuriziyo@gmail.com>
2023-05-31 11:05:17 +0500

    created

---
+++ lib/ui/bottom_ui/shopping_basket_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShoppingBasketPage extends StatefulWidget {
+  const ShoppingBasketPage({Key? key}) : super(key: key);
+
+  @override
+  State<ShoppingBasketPage> createState() => _ShoppingBasketPageState();
+}
+
+class _ShoppingBasketPageState extends State<ShoppingBasketPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
